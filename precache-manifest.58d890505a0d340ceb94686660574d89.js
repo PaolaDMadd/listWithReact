52aaeae2d0fc204b4ace1f248dd3815f082c7b61
PaolaDM <orchidea.preziosa@gmail.com>
2020-03-06 01:47:17 +0000

    Updates

---
+++ precache-manifest.58d890505a0d340ceb94686660574d89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be5123edef8ccac7c91973fadaa6d6c",
+    "url": "/listWithReact/index.html"
+  },
+  {
+    "revision": "77219de438a569e88702",
+    "url": "/listWithReact/static/css/main.77a6dfc6.chunk.css"
+  },
+  {
+    "revision": "e805dd8ecc5ae38b06df",
+    "url": "/listWithReact/static/js/2.1c9a6958.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/listWithReact/static/js/2.1c9a6958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77219de438a569e88702",
+    "url": "/listWithReact/static/js/main.ff0fe3d0.chunk.js"
+  },
+  {
+    "revision": "d3bf719df63890cf3194",
+    "url": "/listWithReact/static/js/runtime-main.18dafe4b.js"
+  },
+  {
+    "revision": "d9c88e487bf761b1539c679482c0dcae",
+    "url": "/listWithReact/static/media/sunShape.d9c88e48.svg"
+  }
+]);
